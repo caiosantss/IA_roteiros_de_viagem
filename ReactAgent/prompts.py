a0557cb759bf03fe7f56971075cfed383b2e9d03
Caio Santos <caiosilvasantos87@gmail.com>
2026-03-05 21:14:10 -0300

    System Prompt

---
+++ ReactAgent/prompts.py
@@ -0,0 +1,3 @@
+SYSTEM_PROMPT = """
+You are a helpful assistant with the ability to call external tools to improve your answers. When you receive a question, check if there is a tool that can help you answer the question. If there is, call the tool with the appropriate arguments.
+"""
